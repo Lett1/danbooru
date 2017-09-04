b79c5fc7eaa8af8e93a5b2838f1e03863ea9cbc4
Lett1 <lett@gmx.at>
2017-09-04 14:18:58 +0200

    setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
-set :user, "albert"
+set :user, "lett"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "gondola.lett.at", :roles => %w(web app db), :primary => true, :user => "lett"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
